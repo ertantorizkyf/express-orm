61714f1a4e3f7c0eb8eebc3e238dd9b14ce2a051
Rizky Ertanto <ertantorizkyf@gmail.com>
2024-01-17 16:47:54 +0700

    Generate token after login

--- server/helpers/authHelper.js
+++ server/helpers/authHelper.js
@@ -1,15 +1,30 @@
 const Boom = require('boom');
 const bcrypt = require('bcryptjs');
 const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+
 const db = require('../../models');
 const GeneralHelper = require('./generalHelper');
 
+const jwtSecretToken = process.env.JWT_SECRET_TOKEN || 'super_strong_key';
+const jwtExpiresIn = process.env.JWT_EXPIRES_IN || '24h';
 const fileName = 'server/helpers/authHelper.js';
 const salt = bcrypt.genSaltSync(10);
 
-const __hashPassword = (password) => bcrypt.hashSync(password, salt);
+// eslint-disable-next-line arrow-body-style
+const __hashPassword = (password) => {
+  return bcrypt.hashSync(password, salt);
+}
 
-const __comparePassword = (payloadPass, dbPass) => bcrypt.compareSync(payloadPass, dbPass);
+// eslint-disable-next-line arrow-body-style
+const __comparePassword = (payloadPass, dbPass) => {
+  return bcrypt.compareSync(payloadPass, dbPass);
+}
+
+// eslint-disable-next-line arrow-body-style
+const __generateToken = (data) => {
+  return jwt.sign(data, jwtSecretToken, { expiresIn: jwtExpiresIn });
+}
 
 const registerUser = async (dataObject) => {
   const { name, email, password } = dataObject;
@@ -40,8 +55,13 @@ const login = async (dataObject) => {
     if(!isPassMatched) {
       return Promise.reject(Boom.badRequest('WRONG_CREDENTIALS'));
     }
+
+    const token = __generateToken({
+      name: user.name,
+      password: user.password
+    });
     
-    return Promise.resolve(true);
+    return Promise.resolve({ token });
   } catch (err) {
     console.log([fileName, 'login', 'ERROR'], { info: `${err}` });
     return Promise.reject(GeneralHelper.errorResponse(err));
